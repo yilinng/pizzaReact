52310a65a07b46a0497aa36230704c78cff20dd5
yilinng <wei9912@gmail.com>
2021-04-16 23:07:06 +0800

    update total on orderItems

--- src/components/OrderDetail.js
+++ src/components/OrderDetail.js
@@ -1,23 +1,21 @@
 import React from 'react';
 
 const OrderDetail = ({handle}) => {
-	
+
+
 	return(
-		<div className="orderItem w-2/4 h-auto bg-gray-200 my-20 mx-auto">			
-			{handle ?(
+		<div className="orderItem w-2/4 overflow-auto h-32 m-auto">			
+			{handle &&
 				handle.map(cart => (
-					<ul className="cart list-disc" key={cart.id}>
+					<ul className="cart list-disc h-auto bg-gray-100" key={cart.id}>
 						<li className="orderDetal flex justify-between">
 							<span className="w-32 font-sans text-base font-medium">{cart.title}</span>
 							<span>X{cart.count}</span>
 							<span className="lining-nums">${cart.price}</span>
 						</li>
-					</ul>	
-			))
-			):(
-				<span className="m-auto font-sans text-xl font-medium">no order</span>
+					</ul>
+				)
 			)}
-
 		</div>
 	)
 }
--- src/components/OrderItems.js
+++ src/components/OrderItems.js
@@ -4,20 +4,32 @@ import ProgressBar from "./ui/ProgressBar";
 
 const OrderItems = ({handleSubmit}) => {
     
+    	
+	//get total cost
+	const getTotal = ((handle) => {
+        const res = Array.from(handle).reduce((prev, item) => {
+            return prev + (item.price * item.count);
+        },0)
+		return res;
+    });
+
     return (
         <div className="orderItems my-20 mx-6 w-5/6">
             <header className="page-header">
                 <span className="text-2xl text-blue-700 font-extrabold">OrderList</span>
             </header>
             
-            {handleSubmit && 
+            {handleSubmit.length ?
                 handleSubmit.map((handle, index) => (
-                    <article className="orderItem w-3/4 h-auto rounded-lg border-2 border-gray-300 my-5" key={index}>
+                    <article className="orderItem w-3/4 h-72 rounded-lg border-2 border-gray-300 my-5" key={index}>
                         <span className="text-xl font-bold mx-10">We're are processing your order</span>
                         <ProgressBar/>
                         <OrderDetail handle={handle} key={index}/>
+                        <div className="flex justify-center">
+                            <span className="text-3xl text-blue-700">Total${getTotal(handle)}</span>
+                        </div>
                     </article>
-            ))} 
+            )): <article>no orderList can see :)</article>} 
             
         </div>
     )
